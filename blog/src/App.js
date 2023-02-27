013b8da4d63902e7725eccd377e1dce83928c02c
Zbigniew Kulczński <z-kulczynski@o2.pl>
2023-02-27 18:22:39 +0100

    code added

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
+import Navbar from './Navbar';
 
 function App() {
   const title = "Welcome to my blog";
-  const likes = 50;
+  
 
 	return (
 		<div className="App">
+      <Navbar />
 			<div className="content">
 				<h1>{ title }</h1>
-        <p>Liked { likes} times</p>
 			</div>
 		</div>
 	);
