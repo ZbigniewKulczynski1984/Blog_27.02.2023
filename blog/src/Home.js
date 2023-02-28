33dea6ca72f7e1368785af8a617c711beac083c8
Zbigniew Kulczński <z-kulczynski@o2.pl>
2023-02-28 14:49:35 +0100

    code added

--- blog/src/Home.js
+++ blog/src/Home.js
@@ -1,18 +1,25 @@
 import React from 'react';
-import { useState} from 'react'
-
+import { useState } from 'react';
 
 const Home = () => {
-    const [name, setName] = useState('Zbys');
-
-	const handleClick = () => {
-		setName('Aga')
-	};
+	const [blogs, setBlogs] = useState([
+		{ title: 'My new website', body: 'lorem ipsum...', author: 'Zbys', id: 1 },
+		{ title: 'Welcome party!', body: 'lorem ipsum...', author: 'Aga', id: 2 },
+		{
+			title: 'We want holliday',
+			body: 'lorem ipsum...',
+			author: 'Goga',
+			id: 3,
+		},
+	]);
 	return (
 		<div className="home">
-			<h2>Homepage</h2>
-			<p>{name}</p>
-			<button onClick={handleClick}>Click me</button>
+			{blogs.map((blog) => (
+				<div className="blog-preview" key={blog.id}>
+					<h2>{blog.title}</h2>
+					<p>Written by {blog.author}</p>
+				</div>
+			))}
 		</div>
 	);
 };
