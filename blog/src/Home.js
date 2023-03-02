8ccef443074628fdc86495cd9df74c8bc8bd9fa1
Zbigniew Kulczński <z-kulczynski@o2.pl>
2023-03-02 14:43:25 +0100

    code added

--- blog/src/Home.js
+++ blog/src/Home.js
@@ -1,30 +1,38 @@
 import React from 'react';
-import { useEffect, useState } from "react";
-import BlogList from "./BlogList";
+import { useEffect, useState } from 'react';
+import BlogList from './BlogList';
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
+	const [blogs, setBlogs] = useState(null);
+	const [isPending, setIsPending] = useState(true);
+	const [error, setError] = useState(null);
 
-  useEffect(() => {
-    setTimeout(() => {
-      fetch('http://localhost:8000/blogs')
-      .then(res => {
-        return res.json();
-      })
-      .then(data => {
-        setIsPending(false);
-        setBlogs(data);
-      })
-    }, 1000);
-  }, [])
+	useEffect(() => {
+		setTimeout(() => {
+			fetch('http://localhost:8000/blogs')
+				.then((res) => {
+					if (!res.ok) {
+						throw Error('could not fetch the data');
+					}
+					return res.json();
+				})
+				.then((data) => {
+					setIsPending(false);
+					setBlogs(data);
+				})
+				.catch((err) => {
+					setError(err.message);
+				});
+		}, 500);
+	}, []);
 
-  return (
-    <div className="home">
-      { isPending && <div>Loading...</div> }
-      {blogs && <BlogList blogs={blogs} />}
-    </div>
-  );
-}
- 
+	return (
+		<div className="home">
+			{error && <div>{error}</div>}
+			{isPending && <div>Loading...</div>}
+			{blogs && <BlogList blogs={blogs} />}
+		</div>
+	);
+};
+
 export default Home;
