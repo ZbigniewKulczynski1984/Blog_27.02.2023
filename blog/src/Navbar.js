7862eb348835f56c78a639d82efeed25089220ac
Zbigniew Kulczński <z-kulczynski@o2.pl>
2023-03-02 15:23:15 +0100

    code added

--- blog/src/Navbar.js
+++ blog/src/Navbar.js
@@ -3,13 +3,17 @@ import React from 'react'
 const Navbar = () => {
   return (
     <nav className="navbar">
-        <h1>My Blog</h1>
-        <div className="links">
-            <a href="/">Home</a>
-            <a href="/create">New Blog</a>
-        </div>
+      <h1>My Blog</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/create" style={{ 
+          color: 'white', 
+          backgroundColor: '#f1356d',
+          borderRadius: '8px' 
+        }}>New Blog</a>
+      </div>
     </nav>
-  )
+  );
 }
-
-export default Navbar
+ 
+export default Navbar;
