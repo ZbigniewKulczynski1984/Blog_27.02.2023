23e15f76f43274277806cb52b0fd8758a17475a0
Zbigniew Kulczński <z-kulczynski@o2.pl>
2023-03-03 12:59:12 +0100

    code added

---
+++ blog/src/BlogDetails.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BlogDetails = () => {
+  return (
+    <div className='blog-details'>
+      <h2>Blog details</h2>
+    </div>
+  )
+}
+
+export default BlogDetails
